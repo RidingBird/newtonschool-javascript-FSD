4c65d55d79034e563dac53ce1da0a2ec3b0b4763
Yogesh Gupta <yg956964@gmail.com>
2024-01-24 21:07:36 +0530

    added new topics like objects and arrays

--- basicsOfJS/script.js
+++ basicsOfJS/script.js
@@ -87,28 +87,85 @@
 // 1. string-number to number:----
 
 // const marks ="19";
-// console.log(+marks);
-// console.log(Number(marks));//always use this method
+// // console.log(+marks);
+// // console.log(Number(marks));//always use this method
 
-let num ="19999999";
-console.log(typeof Number(num));
-console.log("18"*"9");
-console.log("18"-"9");
-console.log("18"/"0");
-console.log("18"%"9");
-console.log("18"+"9"); //because string + string = string
-console.log(typeof (Number("1")+Number("4")));
+// let num ="19999999";
+// console.log(typeof Number(num));
+// console.log("18"*"9");
+// console.log("18"-"9");
+// console.log("18"/"0");
+// console.log("18"%"9");
+// console.log("18"+"9"); //because string + string = string
+// console.log(typeof (Number("1")+Number("4")));
 
-// ================boolean conversion===================
+// // ================boolean conversion===================
 
-console.log(Boolean(100));
-console.log(Boolean(10));
-console.log(Boolean(66));
-console.log(Boolean(0));
-//Boolean(any number ecxept 0) give true
+// console.log(Boolean(100));
+// console.log(Boolean(10));
+// console.log(Boolean(66));
+// console.log(Boolean(0));
+// //Boolean(any number ecxept 0) give true
 
-//string  -->  boolean
+// //string  -->  boolean
 
-Boolean("");//false
-Boolean("gfghdd165654");//true;
+// // Boolean("");//false
+// // Boolean("gfghdd165654");//true;
 
+// let person = {
+//     name: "yogesh gupta",
+//     class: 12,
+//     "hello world":"hey yogesh"
+// }
+
+
+// // person = null;
+
+// // ====GET
+
+// console.log(person);
+// console.log(person["class"]);
+// console.log(person.class);
+
+
+// // ==== edit
+
+// person.class=8;
+// person["hello world"]="jai shree Ram"
+
+// console.log(person.class, "edited class");
+// console.log(person["hello world"], "edited hello world");
+
+// // // =======delete
+
+// // delete person.class;
+// // console.log(person, "person after delete");
+
+// const person2 = person;
+// person=null;
+
+// console.log(person, "person");
+// console.log(person2, "person2");
+
+// // -----------Array-----------------------------
+
+// let arr =[1, 2, 3, 4, 5];
+// console.log(arr);
+
+// let arr1 =[1, "yogesh", 3, "mayank", true];
+// console.log(arr1);
+
+// console.log(arr[1], arr1[1], arr1[arr.length-1]);
+// console.log(arr.length);
+
+// // arr=null;
+// // console.log(arr);
+
+// let newlength=arr1.push("hello");
+
+// console.log(arr1);
+
+
+// let lastElement = arr1.pop();
+// console.log(lastElement);
+// console.log(arr1);
